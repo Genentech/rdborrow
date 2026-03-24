b5f570455d0b15fafecd129072653fafcf3cdce3
Matt Secrest <secrestm@gene.com>
2026-03-24 14:52:14 -0700

    missing dependencies

--- R/package.R
+++ R/package.R
@@ -1,14 +1,15 @@
 #' @import checkmate
-#' @import gsl 
-#' @import copula 
-#' @import futile.logger 
-#' @import mvtnorm 
+#' @import gsl
+#' @import copula
+#' @import futile.logger
+#' @import mvtnorm
 #' @import dplyr
 #' @import boot
 #' @import Matrix
 #' @import CVXR
 #' @import progress
 #' @import future.apply
-
-
+#' @importFrom methods is new
+#' @importFrom stats as.formula glm lm model.matrix predict qnorm rbinom rmultinom var
+#' @importFrom utils data
 NULL
