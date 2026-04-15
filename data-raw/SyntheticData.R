497fe12f6f1a4be29614166e563b9fb8449201c3
Matt Secrest <secrestm@gene.com>
2026-04-15 10:07:31 -0700

    styler

--- data-raw/SyntheticData.R
+++ data-raw/SyntheticData.R
@@ -16,43 +16,43 @@ normal <- copula::normalCopula(param = c(0.8), dim = 4, dispstr = "ar1")
 # x3 = Scoliosis (0 = No, 1 = Yes)
 # x4 = Age_Enrollment
 X_int <- simulate_X_copula(
-   n = 200,
-   p = 4,
-   cp = normal,
-   margins = c("binom", "binom", "binom", "exp"),
-   paramMargins = list(
-      list(size = 1, prob = 0.7),
-      list(size = 1, prob = 0.9),
-      list(size = 1, prob = 0.3),
-      list(rate = 1 / 10)
-   )
+  n = 200,
+  p = 4,
+  cp = normal,
+  margins = c("binom", "binom", "binom", "exp"),
+  paramMargins = list(
+    list(size = 1, prob = 0.7),
+    list(size = 1, prob = 0.9),
+    list(size = 1, prob = 0.3),
+    list(rate = 1 / 10)
+  )
 )
 
 X_int$x4 <- round(X_int$x4) + 1
 # x5 = Baseline outcome (Y0), modeled as linear function of covariates
 X_int$x5 <- 30 + 10 * X_int$x1 + 7 * X_int$x2 +
-   (-6) * X_int$x3 + (-0.5) * X_int$x4 +
-   rnorm(200, mean = 0, sd = 10)
+  (-6) * X_int$x3 + (-0.5) * X_int$x4 +
+  rnorm(200, mean = 0, sd = 10)
 
 # ===== Generate external control covariates =====
 X_ext <- simulate_X_copula(
-   n = 100,
-   p = 4,
-   cp = normal,
-   margins = c("binom", "binom", "binom", "exp"),
-   paramMargins = list(
-      list(size = 1, prob = 0.7),
-      list(size = 1, prob = 0.9),
-      list(size = 1, prob = 0.3),
-      list(rate = 1 / 10)
-   )
+  n = 100,
+  p = 4,
+  cp = normal,
+  margins = c("binom", "binom", "binom", "exp"),
+  paramMargins = list(
+    list(size = 1, prob = 0.7),
+    list(size = 1, prob = 0.9),
+    list(size = 1, prob = 0.3),
+    list(rate = 1 / 10)
+  )
 )
 
 X_ext$x4 <- round(X_ext$x4) + 1
 # External baseline outcome has different intercept/coefficients
 X_ext$x5 <- 50 + 10 * X_ext$x1 + 2 * X_ext$x2 +
-   (-1) * X_ext$x3 + (-0.3) * X_ext$x4 +
-   rnorm(100, mean = 0, sd = 10)
+  (-1) * X_ext$x3 + (-0.3) * X_ext$x4 +
+  rnorm(100, mean = 0, sd = 10)
 
 # ===== Outcome model specifications =====
 varnames <- c("1", paste0("x", 1:5))
@@ -60,48 +60,48 @@ varnames <- c("1", paste0("x", 1:5))
 # Coefficients: intercept, SMA_Type, SMN2_Copy, Scoliosis, Age, Y0
 # Treatment effect (A) and noise SD noted in comments
 model_form_x_t1 <- setNames(
-   c(10.0, 0.05, -1.5, -1.0, -0.2, -0.1), varnames
+  c(10.0, 0.05, -1.5, -1.0, -0.2, -0.1), varnames
 ) # effect = 1.5, sigma = 4.0
 
 model_form_x_t2 <- setNames(
-   c(6.0, 0.5, -0.5, -1.0, -0.3, -0.06), varnames
+  c(6.0, 0.5, -0.5, -1.0, -0.3, -0.06), varnames
 ) # effect = 1.8, sigma = 4.0
 
 model_form_x_t3 <- setNames(
-   c(5.0, 1.9, 1.4, -1.3, -0.4, -0.15), varnames
+  c(5.0, 1.9, 1.4, -1.3, -0.4, -0.15), varnames
 ) # effect = 1.6, sigma = 4.0
 
 model_form_x_t4 <- setNames(
-   c(1.2, 1.0, 2.0, -0.5, -0.4, -0.10), varnames
+  c(1.2, 1.0, 2.0, -0.5, -0.4, -0.10), varnames
 ) # effect = 2.5, sigma = 5.0
 
 outcome_model_specs <- list(
-   list(
-      effect = 0, model_form_x = model_form_x_t1,
-      noise_mean = 0, noise_sd = 4
-   ),
-   list(
-      effect = 1.0, model_form_x = model_form_x_t2,
-      noise_mean = 0, noise_sd = 4
-   ),
-   list(
-      effect = 2.0, model_form_x = model_form_x_t3,
-      noise_mean = 0, noise_sd = 4
-   ),
-   list(
-      effect = 5.0, model_form_x = model_form_x_t4,
-      noise_mean = 0, noise_sd = 4
-   )
+  list(
+    effect = 0, model_form_x = model_form_x_t1,
+    noise_mean = 0, noise_sd = 4
+  ),
+  list(
+    effect = 1.0, model_form_x = model_form_x_t2,
+    noise_mean = 0, noise_sd = 4
+  ),
+  list(
+    effect = 2.0, model_form_x = model_form_x_t3,
+    noise_mean = 0, noise_sd = 4
+  ),
+  list(
+    effect = 5.0, model_form_x = model_form_x_t4,
+    noise_mean = 0, noise_sd = 4
+  )
 )
 
 # ===== Simulate trial data =====
 SyntheticData <- simulate_trial(
-   X_int,
-   X_ext,
-   num_treated = 100,
-   OLE_flag = TRUE,
-   T_cross = 2,
-   outcome_model_specs
+  X_int,
+  X_ext,
+  num_treated = 100,
+  OLE_flag = TRUE,
+  T_cross = 2,
+  outcome_model_specs
 )
 
 # ===== Save to data/ =====
